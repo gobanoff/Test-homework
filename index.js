4ba1241d80bbb8c72a43222b0471b0e16dfa8f9a
gobanoff <a.kliosovas@yahoo.com>
2024-04-24 17:08:13 +0300

    fix

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const uniqueNumbers = new Set();
 
 uniqueNumbers.add(1);
@@ -7,129 +6,124 @@ uniqueNumbers.add(3);
 uniqueNumbers.add(2); // Dublikatas nebus pridėtas
 
 //console.log(uniqueNumbers); // Output: Set(3) { 1, 2, 3 }
-const a =[2,2,3,3,4,4,5,5,5];
+const a = [2, 2, 3, 3, 4, 4, 5, 5, 5];
 function countElementFrequency(arr) {
   const frequencyMap = new Map();
-  arr.forEach(element => {
+  arr.forEach((element) => {
     frequencyMap.set(element, (frequencyMap.get(element) || 0) + 1);
   });
   return frequencyMap;
 }
 
-  //console.log(countElementFrequency(a));
+//console.log(countElementFrequency(a));
 
+const vehicles = ["car", "truck", "car", "bike", "car", "bike", "truck"];
 
-  const vehicles = ['car', 'truck', 'car', 'bike', 'car', 'bike', 'truck'];
-  
 // Call the function to count element frequency
 const elementFrequency = countElementFrequency(vehicles);
 elementFrequency.forEach((count, element) => {
   console.log(`${element}: ${count} time(s)`);
 });
 
-
-
-
-function walkDog(){
+function walkDog() {
   return new Promise((resolve, reject) => {
-      setTimeout(() => {
+    setTimeout(() => {
+      const dogWalked = true;
 
-          const dogWalked = true;
-
-          if(dogWalked){
-              resolve("You walk the dog ");
-          }
-          else{
-              reject("You DIDN'T walk the dog");
-          }
-      }, 1500);
+      if (dogWalked) {
+        resolve("You walk the dog ");
+      } else {
+        reject("You DIDN'T walk the dog");
+      }
+    }, 1500);
   });
 }
 
-function cleanKitchen(){
+function cleanKitchen() {
   return new Promise((resolve, reject) => {
-      setTimeout(() => {
-          
-          const kitchenCleaned = true;
+    setTimeout(() => {
+      const kitchenCleaned = true;
 
-          if(kitchenCleaned){
-              resolve("You clean the kitchen ");
-          }
-          else{
-              reject("You DIDN'T clean the kitchen");
-          }
-      }, 2500);
+      if (kitchenCleaned) {
+        resolve("You clean the kitchen ");
+      } else {
+        reject("You DIDN'T clean the kitchen");
+      }
+    }, 2500);
   });
 }
 
-function takeOutTrash(){
+function takeOutTrash() {
   return new Promise((resolve, reject) => {
-      setTimeout(() => {
+    setTimeout(() => {
+      const trashTakenOut = true;
 
-          const trashTakenOut = true;
-
-          if(trashTakenOut){
-              resolve("You take out the trash ");
-          }
-          else{
-              reject("You DIDN'T take out the trash");
-          }
-
-      }, 500);
+      if (trashTakenOut) {
+        resolve("You take out the trash ");
+      } else {
+        reject("You DIDN'T take out the trash");
+      }
+    }, 500);
   });
 }
 
-function takeOutTrash(){
+function takeOutTrash() {
   return new Promise((resolve, reject) => {
-      setTimeout(() => {
+    setTimeout(() => {
+      const trashTakenOut = true;
 
-          const trashTakenOut = true;
-
-          if(trashTakenOut){
-              resolve("You take out the trash ");
-          }
-          else{
-              reject("You DIDN'T take out the trash");
-          }
-
-      }, 500);
+      if (trashTakenOut) {
+        resolve("You take out the trash ");
+      } else {
+        reject("You DIDN'T take out the trash");
+      }
+    }, 500);
   });
 }
 
-async function doChores(){
-try{
-  const walkDogResult = await walkDog();
-  console.log(walkDogResult);
+async function doChores() {
+  try {
+    const walkDogResult = await walkDog();
+    console.log(walkDogResult);
 
-  const cleanKitchenResult = await cleanKitchen();
-  console.log(cleanKitchenResult);
+    const cleanKitchenResult = await cleanKitchen();
+    console.log(cleanKitchenResult);
 
-  const takeOutTrashResult = await takeOutTrash();
-  console.log(takeOutTrashResult);
-  
-  console.log("You finsihed all the chores!");
+    const takeOutTrashResult = await takeOutTrash();
+    console.log(takeOutTrashResult);
+
+    console.log("You finsihed all the chores!");
+  } catch (error) {
+    console.error(error);
+  }
 }
-catch(error){
-  console.error(error);
-}}
 doChores();
 
 // namu darbas
 
-function generateEmail(length, includeLowercase, includeUppercase, includeNumbers) {
-
-  if ((length < 1) || (!includeLowercase && !includeUppercase && !includeNumbers)) {
-    return console.log("Negaliu sugeneruoti e-mailo. Keliaukit namo(arba čiuožkit).");
+function generateEmail(
+  length,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers
+) {
+  if (
+    length < 1 ||
+    (!includeLowercase && !includeUppercase && !includeNumbers)
+  ) {
+    return console.log(
+      "Negaliu sugeneruoti e-mailo. Keliaukit namo(arba čiuožkit)."
+    );
   }
 
-  let char = '';
-  if (includeLowercase) char += 'abcdefghijklmnopqrstuvwxyz';
-  if (includeUppercase) char += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  if (includeNumbers) char += '0123456789';
+  let char = "";
+  if (includeLowercase) char += "abcdefghijklmnopqrstuvwxyz";
+  if (includeUppercase) char += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeNumbers) char += "0123456789";
 
-  let email = '';
+  let email = "";
   for (let i = 0; i < length; i++) {
-      email += char.charAt(Math.floor(Math.random() * char.length));
+    email += char.charAt(Math.floor(Math.random() * char.length));
   }
   return email;
 }
@@ -145,6 +139,11 @@ const includeLowercase = false;
 const includeUppercase = false;
 const includeNumbers = false;
 
-const email = generateEmail(emailNameLength, includeLowercase, includeUppercase, includeNumbers);
+const email = generateEmail(
+  emailNameLength,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers
+);
 
 console.log(`Generated email: ${email}@bit.lt`);
